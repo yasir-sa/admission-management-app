7bad62dd35524293de9af4bb6462d40120eb6834
asiyasa <asiyasa07@gmail.com>
2026-07-09 19:21:47 +0530

    Force IPv4 for Gmail SMTP to fix ENETUNREACH OTP send failures
    Node's default DNS resolution was picking Gmail's IPv6 address,
    which isn't reachable on networks without full IPv6 connectivity.

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,7 +1,10 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
